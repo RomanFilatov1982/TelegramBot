ad83b7ebfd12a18e26e978590f1a9f64b8f676d0
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-07-27 14:17:36 +0300

    changed browserVersion

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,11 +18,12 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com/");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "138.0");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.timeout = 10000;
         //Configuration.holdBrowserOpen = true;
         Configuration.remote = System.getProperty("remoteUrl");
-        //Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        //срфтпувConfiguration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
